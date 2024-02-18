51bb3ac6f6ed401e4080b1915e475438530fa524
Sushant Kope <159564477+kope264@users.noreply.github.com>
2024-02-18 22:02:54 +0530

    Update reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,12 +1,12 @@
 package strings;
 import java.util.Scanner;
-
+//its just asimple java proagram which gives output of the sting in reverses order 
 public class reverseString {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String x= sc.nextLine();
         StringBuilder sb =new StringBuilder(x);
-        for(int i=0;i<sb.length()/2;i++){
+        for(int i=0;i<sb.length()/2;i++){  
             int front=i;
             int back=sb.length()-1-i;
 
